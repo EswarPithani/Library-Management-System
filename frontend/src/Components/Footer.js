6dd77c0522da3c9f1e082ff22a84f5b756e56a54
EswarPithani <151363925+EswarPithani@users.noreply.github.com>
2025-04-19 12:06:24 +0530

    Initial commit of Library Management System project

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -14,14 +14,13 @@ function Footer() {
                     <div className="contact-details">
                         <h1>Contact Us</h1>
                         <p>Librarian</p>
-                        <p>Government School</p>
-                        <p>Visakhapatnam-530041</p>
-                        <p>Andhra Pradesh</p>
+                        <p>NIT, Andhra Pradesh</p>
+                        <p>Tadepalligudem</p>
                         <p>India</p>
-                        <p><b>Email:</b>example@gmail.com</p>
+                        <p><b>Email:</b>rollno@student.nitandhra.ac.in</p>
                     </div>
                     <div className='usefull-links'>
-                        <h1>Usefull Links</h1>
+                        <h1>Usefull Links</h1>  
                         <a href='#home'>Link-1</a>
                         <a href='#home'>Link-1</a>
                         <a href='#home'>Link-1</a>
@@ -31,7 +30,7 @@ function Footer() {
                         <h1>Librarian</h1>
                         <p>Name</p>
                         <p>Education</p>
-                        <p>Contact: +91 9123456787</p>
+                        <p>Contact: +91 123545786</p>
                     </div>
                 </div>
                 <div className="contact-social" >
@@ -42,7 +41,7 @@ function Footer() {
                 </div>
             </div>
             <div className='copyright-details'>
-                <p className='footer-copyright'>&#169; 2020 copyright all right reserved<br /><span>Designed with ❤️ by Pranavdhar Reddy</span></p>
+                <p className='footer-copyright'>&#169; 2025 copyright all right reserved<br /></p>
             </div>
         </div>
     )
--- frontend/src/Components/PhotoGallery.js
+++ frontend/src/Components/PhotoGallery.js
@@ -7,16 +7,17 @@ function PhotoGallery() {
         <div className='photogallery-container'>
             <h1 className='photogallery-title'>Photo Gallery</h1>
             <div className="photogallery-images">
-                <img src="https://source.unsplash.com/1300x1200/?algeria" alt=''/>
-                <img src="https://source.unsplash.com/1300x1200/?lebanon" alt=''/>
-                <img src="https://source.unsplash.com/1300x1200/?qatar" alt=''/>
-                <img src="https://source.unsplash.com/1300x1200/?uae" alt=''/>
-                <img src="https://source.unsplash.com/1300x1200/?kuwait" alt=''/>
-                <img src="https://source.unsplash.com/1300x1200/?oman" alt=''/>
-                <img src="https://source.unsplash.com/1300x1200/?turkey" alt=''/>
-                <img src="https://source.unsplash.com/1300x1200/?iran" alt=''/>
-                <img src="https://source.unsplash.com/1300x1200/?jordan" alt=''/>
-                <img src="https://source.unsplash.com/1300x1200/?Kuwait" alt=''/>
+            <img src="https://images.unsplash.com/photo-1521587760476-6c12a4b040da?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGlicmFyeXxlbnwwfHwwfHx8MA%3D%3D" alt="Algeria" />
+            <img src="https://images.unsplash.com/photo-1568667256549-094345857637?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bGlicmFyeXxlbnwwfHwwfHx8MA%3D%3D" alt="Lebanon" />
+            <img src="https://plus.unsplash.com/premium_photo-1677567996070-68fa4181775a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bGlicmFyeXxlbnwwfHwwfHx8MA%3D%3D" alt="Qatar" />
+            <img src="https://images.unsplash.com/photo-1580537659466-0a9bfa916a54?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8bGlicmFyeXxlbnwwfHwwfHx8MA%3D%3D" alt="UAE" />
+            <img src="https://images.unsplash.com/photo-1535905557558-afc4877a26fc?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bGlicmFyeXxlbnwwfHwwfHx8MA%3D%3D" alt="Kuwait" />
+            <img src="https://images.unsplash.com/photo-1614849963640-9cc74b2a826f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGxpYnJhcnl8ZW58MHx8MHx8fDA%3D" alt="Oman" />
+            <img src="https://plus.unsplash.com/premium_photo-1664300897489-fd98eee64faf?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8bGlicmFyeXxlbnwwfHwwfHx8MA%3D%3D" alt="Turkey" />
+            <img src="https://images.unsplash.com/photo-1544640808-32ca72ac7f37?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGxpYnJhcnl8ZW58MHx8MHx8fDA%3D" alt="Iran" />
+            <img src="https://images.unsplash.com/photo-1585779034823-7e9ac8faec70?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGxpYnJhcnl8ZW58MHx8MHx8fDA%3D" alt="Jordan" />
+            <img src="https://images.unsplash.com/photo-1600431521340-491eca880813?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGxpYnJhcnl8ZW58MHx8MHx8fDA%3D" alt="Kuwait" />
+
             </div>
             <button>VIEW MORE<ArrowForwardIosIcon style={{fontSize:20}}/></button>
         </div>
